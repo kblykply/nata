5091417f1493ff795849ddfbf29be25eeee9813e
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-04-28 11:44:23 +0300

    add chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-
+import Script from 'next/script'
 
 
 
@@ -18,7 +18,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr" className={"scroll-smooth "}>
+
+
+
+<head>
+        {/* Zoho SalesIQ Initialization Script */}
+        <Script id="zoho-salesiq-init" strategy="afterInteractive">
+          {`
+            window.$zoho = window.$zoho || {};
+            $zoho.salesiq = $zoho.salesiq || { ready: function() {} };
+          `}
+        </Script>
+
+        {/* Zoho SalesIQ Widget Script */}
+        <Script
+          id="zsiqscript"
+          src="https://salesiq.zohopublic.eu/widget?wc=siq12bbb3f71233b881580f172590a15f3e21a37acd0a5d6482713a01047b753e48"
+          strategy="lazyOnload"
+        />
+      </head>
+
+
       <body className="antialiased">
+
+
+
         <Header />
         {children}
         <Footer />
