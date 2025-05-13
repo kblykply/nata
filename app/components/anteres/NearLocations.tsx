3291e35f061ff88d2aa0788ac2c7e2bec384da7d
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-05-13 15:56:00 +0300

    map api

--- app/components/anteres/NearLocations.tsx
+++ app/components/anteres/NearLocations.tsx
@@ -207,6 +207,9 @@ export default function NearbyMap() {
 const { isLoaded } = useJsApiLoader({
   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
 });
+
+
+
   const filteredPlaces =
     selectedCategory === "all"
       ? places
