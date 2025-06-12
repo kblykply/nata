23353f4457c40af51d58adba2dabbc143b527d27
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-06-12 12:35:22 +0300

    Initial commit

--- app/components/about-us/Hero.tsx
+++ app/components/about-us/Hero.tsx
@@ -4,21 +4,22 @@ import Image from 'next/image';
 
 const AboutSection = () => {
   return (
-    <div className="flex justify-center items-center p-10 bg-white">
-      <div className="relative w-full max-w-7xl h-64 rounded-xl overflow-hidden">
-        <Image
-          src="/natayasamhakkinda.png"  // Place your image in the public folder with this name
-          alt="About Nata Yaşam"
-          layout="fill"
-          objectFit="cover"
-          className="opacity-100"
-        />
-        <div className="absolute inset-0 flex flex-col justify-end p-10 items-center text-white text-center">
-          <h2 className="text-4xl font-semibold">Nata Yaşam</h2>
-          <p className="text-3xl mt-2">Hakkında</p>
-        </div>
-      </div>
+<div className="px-6 md:px-30 py-10">
+  <div className="relative w-full h-64 rounded-2xl overflow-hidden">
+    <Image
+      src="/natayasamhakkinda.png"
+      alt="About Nata Yaşam"
+      layout="fill"
+      objectFit="cover"
+    />
+    <div className="absolute inset-0 flex flex-col justify-end p-10 items-center text-white text-center">
+      <h2 className="text-4xl font-semibold">Nata Yaşam</h2>
+      <p className="text-3xl mt-2">Hakkında</p>
     </div>
+  </div>
+</div>
+
+
   );
 };
 
