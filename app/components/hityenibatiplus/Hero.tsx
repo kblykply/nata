7fd8f90bee63792df88c82f81ca40ced2db28040
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-07-02 17:41:28 +0300

    Initial commit

--- app/components/hityenibatiplus/Hero.tsx
+++ app/components/hityenibatiplus/Hero.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 
 export default function ProjectHeroCloudSection() {
   return (
-    <section  className="relative w-full h-[90vh] md:h-[150vh] overflow-hidden bg-gradient-to-b from-[#006d9d] to-[#e6f3fd]">
+    <section  className="relative w-full h-[90vh] md:h-[140vh] overflow-hidden bg-gradient-to-b from-[#006d9d] to-[#e6f3fd]">
       {/* Looping clouds (seamless illusion) */}
-      <div className="absolute top-60 left-0 w-full h-full z-10 overflow-hidden pointer-events-none">
+      <div className="absolute top-5 left-0 w-full h-full z-10 overflow-hidden pointer-events-none">
         <motion.div
           className="flex h-full w-[200%]"
           animate={{ x: ["0%", "-50%"] }}
@@ -35,7 +35,7 @@ export default function ProjectHeroCloudSection() {
       </div>
 
       {/* Building image */}
-      <div className="absolute bottom-0 left-0 w-full h-[140vh] z-20 pointer-events-none">
+      <div className="absolute bottom-0 left-0 w-full h-[100vh] z-20 pointer-events-none">
         <Image
           src="/yenibatıplusbuild.png"
           alt="Building"
