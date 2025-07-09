041327099cde035564013c6e73dd335b169065ca
Kubilay Kuplay <kubilaykuplay@gmail.com>
2025-07-09 13:17:54 +0300

    💄 Added animated gradient background to feature section

--- app/components/FavoriteButton.tsx
+++ app/components/FavoriteButton.tsx
@@ -14,25 +14,25 @@ export default function FavoriteButton() {
 
   return (   
 
-<Link
-  href="/favorites"
-  onClick={handleClick}
-  className="w-11 h-11 bg-gray-100 rounded-full flex items-center justify-center relative"
->
-  {/* Ping animation */}
-  {showPing && (
-    <span className="absolute inline-flex h-6 w-6 rounded-full bg-red-400 opacity-75 animate-ping"></span>
-  )}
+  <Link
+    href="/favorites"
+    onClick={handleClick}
+    className="w-11 h-11 bg-gray-100 rounded-full flex items-center justify-center relative"
+  >
+    {/* Ping animation */}
+    {showPing && (
+      <span className="absolute inline-flex h-6 w-6 rounded-full bg-red-400 opacity-75 animate-ping"></span>
+    )}
 
-  {/* Icon (smaller than background) */}
-  <Image
-    src="/favori icon.png"
-    alt="Favori"
-    width={20}
-    height={20}
-    className="z-10"
-  />
-</Link>
+    {/* Icon (smaller than background) */}
+    <Image
+      src="/favori icon.png"
+      alt="Favori"
+      width={20}
+      height={20}
+      className="z-10"
+    />
+  </Link>
 
 
   );
